fdc2e6fd51b251ac54e4068c91ac4ce14331b9c6
Victor Chabbert <victorchabbert@users.noreply.github.com>
2019-09-24 11:27:36 +0200

    Fetch office listing with search and display the results

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect } from "react";
 import "./styles/global.scss";
 import styles from "./app.module.scss";
 import Header from "./templates/header";
 import { Office } from "./models/offices";
+import { OfficeSearch } from "./models/offices";
+import OfficeList from "./templates/office-list/OfficeList";
+import { getStats } from "./services/offices";
+import { getOffices } from "./services/offices";
+import StatsBar from "./templates/stats-bar/StatsBar";
+import { Stat } from "./models/stats";
 
 type Sort = "asc" | "desc";
 
 const App: FC = () => {
   const [sort, setSort] = useState<Sort>("asc");
+  const [offices, setOffices] = useState<OfficeSearch | undefined>(undefined);
+  const [query, setQuery] = useState<any>(undefined);
+  const [stats, setStats] = useState<Stat | undefined>(undefined);
 
   const handleSortToggle = () => {
     setSort(sort === "asc" ? "desc" : "asc");
@@ -21,13 +30,38 @@ const App: FC = () => {
     }
   };
 
+  useEffect(() => {
+    getOffices().then((offices) => setOffices(() => offices));
+    getStats().then((stats) => setStats(stats));
+
+    if (query !== undefined) {
+      handleOfficeSearch(query);
+    }
+  }, [query]);
+
+  // componentWillUpdate(_nextProps: any, nextState: State) {
+  //   if (nextState.query !== this.state.query && nextState.query !== undefined) {
+  //     handleOfficeSearch(nextState.query);
+  //   }
+  // }
+
+  const handleOfficeSearch = (query: string) => {
+    getOffices(query).then((offices) => setOffices(() => offices));
+  };
+
   return (
     <div className={styles.app}>
       <Header
         sort={sort}
         onSortToggle={handleSortToggle}
-        onSearchSubmit={console.log}
+        onSearchSubmit={(query) => setQuery(query)}
       />
+      {stats && <StatsBar stats={stats} />}
+      {offices ? (
+        <OfficeList offices={offices.data.sort(sortOffices)} />
+      ) : (
+        false
+      )}
     </div>
   );
 };
